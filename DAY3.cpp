4ef2651818d7f944a052327b4bcb16e5f33df6d0
RAJESH G R <rgr98624@gmail.com>
2026-06-18 10:50:46 +0530

    Implement prime number check in DAY3.cpp

---
+++ DAY3.cpp
@@ -0,0 +1,31 @@
+CHECK  PRIME OR NOOT
+
+#include<iostream>
+using namespace std;
+
+int main(){
+  int i,n;
+  bool isPrime=true;
+  cout<<"Enter a number:";
+  cin>>n;
+
+  if(n<=1){
+     isPrime=false;
+  }
+  else{
+      for(i=2;i<=n/2;i++){
+          if(n%i==0){
+              isPrime=false;
+              break;
+          }
+          else{
+              isPrime=true;
+          }
+      }
+  if(isPrime){
+      cout<<"Prime";
+  }else{
+      cout<<"Not Prime";
+  }
+return 0;
+  }
