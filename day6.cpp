997b9e10612bd7ab3d46943d9ac59935676909d2
RAJESH G R <rgr98624@gmail.com>
2026-06-22 21:23:47 +0530

    Implement element search in array
    Added functionality to search for an element in an array.

--- day6.cpp
+++ day6.cpp
@@ -40,3 +40,36 @@ Second largest elements
 Second largest elements in the array:3
 
 === Code Execution Successful ===
+
+SEARCHING
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[50],i,n,k;
+    bool found=false;
+    
+    cout<<"Enter the number of elements:";
+    cin>>n;
+    
+    cout<<"Enter the elements:";
+    for(i=0;i<n;i++){
+        cin>>arr[i];
+    };
+    
+    cout<<"Enter the element to search:";
+    cin>>k;
+    
+    for(i=0;i<n;i++){
+        if(arr[i]==k){
+            found=true;
+            cout<<"Element found at:"<<i;
+            break;
+        };
+    };
+    if(!found){
+        cout<<"Elent not found in array";
+    };
+    return 0;
+}
