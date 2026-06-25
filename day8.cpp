3652d411939d0842e025992514c9d23b17d9b4ce
RAJESH G R <rgr98624@gmail.com>
2026-06-25 23:12:27 +0530

    Add functionality to reverse an array

--- day8.cpp
+++ day8.cpp
@@ -28,3 +28,25 @@ Enter elements:1 2 3 4 5
 Average of array elements:3
 
 === Code Execution Successful ===
+
+REVERSE ARRAY
+#include<iostream>
+ using namespace std;
+ 
+ int main() {
+     int arr[50],i,n;
+     
+     cout<<"Enter a number of elements:";
+     cin>>n;
+     
+     cout<<"Enter elements:";
+     for(i=0;i<n;i++){
+         cin>>arr[i];
+     }
+     cout<<"Reverse array is:";
+     for(i=n-1;i>=0;i--){
+         cout<<arr[i];
+     }
+     return 0;
+     
+ }
