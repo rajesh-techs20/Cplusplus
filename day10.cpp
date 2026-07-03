e51e2859dfb2b5ca8861de84569e92b53d5a2db4
RAJESH G R <rgr98624@gmail.com>
2026-07-04 00:18:42 +0530

    Add output example for matrix input

--- day10.cpp
+++ day10.cpp
@@ -26,3 +26,14 @@ int main() {
     }
     return 0;
 }
+
+OUTPUT
+Enter the number of elements in rows:4
+Enter the number of elements in columns:2
+Enter the elements:1 2 3 4 5 6 7 8
+
+ Matrix 
+1 2 
+3 4 
+5 6 
+7 8 
