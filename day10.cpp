247083fff9025ef1e53f876885c9c454b168bfce
RAJESH G R <rgr98624@gmail.com>
2026-07-04 00:17:49 +0530

    Add 2D array input and output functionality

---
+++ day10.cpp
@@ -0,0 +1,28 @@
+2D ARRAY
+
+#include<iostream>
+using namespace std;
+int main() {
+    int arr[10][10],r,c,i,j;
+    
+    cout<<"Enter the number of elements in rows:";
+    cin>>r;
+    
+    cout<<"Enter the number of elements in columns:";
+    cin>>c;
+    
+    cout<<"Enter the elements:";
+    for(i=0;i<r;i++){
+        for(j=0;j<c;j++){
+            cin>>arr[i][j];
+        }
+    }
+    cout<<"\n Matrix \n";
+    for(i=0;i<r;i++){
+        for(j=0;j<c;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
