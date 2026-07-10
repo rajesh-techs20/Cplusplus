d87773afc2f6841429280dc2c1c924325ef91629
RAJESH G R <rgr98624@gmail.com>
2026-07-10 21:23:33 +0530

    Implement matrix subtraction in day11.cpp

---
+++ day11.cpp
@@ -0,0 +1,42 @@
+SUBTRACTION OF TWO MATRIX
+
+#include<iostream>
+using namespace std;
+int main() {
+    int a[10][10],b[10][10],c[10][10],i,j,r,k;
+    
+    cout<<"Enter the number of rows:";
+    cin>>r;
+    
+    cout<<"Enter the number of columns:";
+    cin>>k;
+    
+    cout<<"Enter the elements in to first matrix:";
+    for(i=0;i<r;i++){
+        for(j=0;j<k;j++){
+            cin>>a[i][j];
+        }
+    }
+    
+    cout<<"Enter the elements in to second matrix:";
+    for(i=0;i<r;i++){
+        for(j=0;j<k;j++){
+            cin>>b[i][j];
+        }
+    }
+    
+    cout<<"\nsubstraction of two matrix\n";
+    for(i=0;i<r;i++){
+        for(j=0;j<k;j++){
+            c[i][j]=a[i][j]-b[i][j];
+        }
+    }
+    
+    for(i=0;i<r;i++){
+        for(j=0;j<k;j++){
+            cout<<c[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
