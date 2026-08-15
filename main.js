160535c2bee02a12f63a43b6004c3c08bec32bfd
RAJESH G R <rgr98624@gmail.com>
2026-08-15 22:55:54 +0530

    Add functionality to input and store marks

---
+++ main.js
@@ -0,0 +1,8 @@
+let n = Number(prompt("Enter the number of marks:"));
+let marks = [];
+
+for(let i=1;i<=n;i++){
+    let number = Number(prompt("Enter the numbers:"));
+    marks.push(number);
+}
+console.log(marks);
